b2e871ea6095148ce703894626103fe61bce1b52
Jordan Taylor <jordandtaylor@gmail.com>
2015-09-12 01:46:37 -0600

    Added basic view and front end layout

--- SimpleBlog/Controllers/AuthController.cs
+++ SimpleBlog/Controllers/AuthController.cs
@@ -11,7 +11,7 @@ namespace SimpleBlog.Controllers
         // GET: Auth
         public ActionResult Login()
         {
-            return Content( "Login!");
+            return View();
         }
     }
 }
--- SimpleBlog/Controllers/PostsController.cs
+++ SimpleBlog/Controllers/PostsController.cs
@@ -10,7 +10,7 @@ namespace SimpleBlog.Controllers
     {
         public ActionResult Index()
         {
-            return Content("Hello World!");
+            return View( );
         }
     }
 }

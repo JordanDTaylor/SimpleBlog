c5636917a634457c638423c75c794fd7a6bd5c74
Jordan Taylor <jordandtaylor@gmail.com>
2015-09-13 17:49:28 -0600

    Added  basic login form.

--- SimpleBlog/Controllers/AuthController.cs
+++ SimpleBlog/Controllers/AuthController.cs
@@ -3,15 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SimpleBlog.ViewModels;
 
 namespace SimpleBlog.Controllers
 {
     public class AuthController : Controller
     {
-        // GET: Auth
         public ActionResult Login()
         {
-            return View();
+            return View(new AuthLogin {});
+        }
+
+        [HttpPost]
+        public ActionResult Login(AuthLogin form)
+        {
+            if (!ModelState.IsValid)
+                return View(form);
+            if (form.Username != "rainbow dash")
+            {
+                ModelState.AddModelError("Username", "Username or password ins't 20% cooler.");
+                return View(form);
+            }
+            return Content("Form is Valid!");
         }
     }
 }
---
+++ SimpleBlog/ViewModels/Auth.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimpleBlog.ViewModels
+{
+    //Contract between the view and the controller
+    public class AuthLogin
+    {
+        //Must add an actionResult in the controller.
+        [Required]
+        public string Username { get; set; }
+
+        [Required, DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}

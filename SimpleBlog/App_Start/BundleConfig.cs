65d35744ac98879702856f85aee9b18d06dd3257
Jordan Taylor <jordandtaylor@gmail.com>
2015-09-13 20:22:08 -0600

    Imported styles and scripts(jQuery and Bootstrap)

---
+++ SimpleBlog/App_Start/BundleConfig.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Optimization;
+
+namespace SimpleBlog
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/admin/styles")
+                .Include("~/content/styles/bootstrap.css")
+                .Include("~/content/styles/admin.css"));
+
+            bundles.Add(new StyleBundle("~/styles")
+               .Include("~/content/styles/bootstrap.css")
+               .Include("~/content/styles/site.css"));
+
+            bundles.Add(new ScriptBundle("~/admin/scripts")
+                .Include("~/scripts/jquery-2.1.4.js")
+                .Include("~/scripts/jquery.validate.js")
+                .Include("~/scripts/jquery.validate.unobtrusive.js")
+                .Include("~/scripts/bootstrap.js"));
+
+            bundles.Add(new ScriptBundle("~/scripts")
+                .Include("~/scripts/jquery-2.1.4.js")
+                .Include("~/scripts/jquery.validate.js")
+                .Include("~/scripts/jquery.validate.unobtrusive.js")
+                .Include("~/scripts/bootstrap.js"));
+
+        }
+    }
+}

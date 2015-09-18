7ffb13565c2e325fc2767ec16f5e67692e47cabc
Jordan Taylor <jordandtaylor@gmail.com>
2015-09-17 21:59:18 -0600

    Create new and edit actions for posts controller

--- SimpleBlog/Areas/Admin/Controllers/PostsController.cs
+++ SimpleBlog/Areas/Admin/Controllers/PostsController.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Web.Mvc;
 using NHibernate.Linq;
 using SimpleBlog.Areas.Admin.ViewModels;
@@ -28,6 +29,69 @@ namespace SimpleBlog.Areas.Admin.Controllers
             {
                 Posts = new PagedData<Post>(currentPostPage, totalPostsCount, page, PostsPerPage)
             });
+        }
+
+        public ActionResult New()
+        {
+            return View("Form", new PostsForm
+            {
+                IsNew = true
+            });
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var post = Database.Session.Load<Post>(id);
+
+            if (post == null)
+                return HttpNotFound();
+
+            return View("Form", new PostsForm
+            {
+                IsNew = false,
+                PostId = id,
+                Content = post.Content,
+                Slug = post.Slug,
+                Title = post.Title
+
+            });
+
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public ActionResult Form(PostsForm form)
+        {
+            form.IsNew = form.PostId == null;
+
+            if (!ModelState.IsValid)
+                return View(form);
+
+            Post post;
+            if (form.IsNew)
+            {
+                post = new Post
+                {
+                    CreatedAt = DateTime.UtcNow,
+                    User = Auth.User
+                };
+            }
+            else
+            {
+                post = Database.Session.Load<Post>(form.PostId);
+
+                if(post == null)
+                    return HttpNotFound();
+
+                post.UpdatedAt = DateTime.UtcNow;
+            }
+
+            post.Title = form.Title;
+            post.Slug = form.Slug;
+            post.Content = form.Content;
+
+            Database.Session.SaveOrUpdate(post);
+
+            return RedirectToAction("index");
         }
     }
 }
--- SimpleBlog/Areas/Admin/ViewModels/Posts.cs
+++ SimpleBlog/Areas/Admin/ViewModels/Posts.cs
@@ -1,4 +1,5 @@
-﻿using SimpleBlog.Infrastructure;
+﻿using System.ComponentModel.DataAnnotations;
+using SimpleBlog.Infrastructure;
 using SimpleBlog.Models;
 
 namespace SimpleBlog.Areas.Admin.ViewModels
@@ -6,6 +7,22 @@ namespace SimpleBlog.Areas.Admin.ViewModels
     public class PostsIndex
     {
         public PagedData<Post> Posts { get; set; }
+    }
+
+    public class PostsForm
+    {
+        public bool IsNew { get; set; }
+        public int? PostId { get; set; }
+
+        [Required, MaxLength(128)]
+        public string Title { get; set; }
+
+        [Required, MaxLength(128)]
+        public string Slug { get; set; }
+
+        [Required, DataType(DataType.MultilineText)]
+        public string Content { get; set; }
+
     }
 
 }
